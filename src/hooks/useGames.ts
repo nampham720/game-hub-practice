5dd1b426d988d4222a99ca03ca81c610dee2ed91
Nam Pham <nampham@Nams-MacBook-Air.local>
2025-03-23 13:58:35 +0200

    refactor - simplify time calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,10 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
-const apiClient = new APIClient<Game>('/games')
+const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
@@ -18,21 +19,19 @@ const useGames = (gameQuery: GameQuery) =>
   useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: ({ pageParam = 1 }) =>
-      apiClient
-        .getAll({
-          params: {
-            genres: gameQuery.genreId,
-            parent_platforms: gameQuery.platformId,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-            page: pageParam
-          },
-        }),
-      getNextPageParam: (lastPage, allPages) => {
-        return lastPage ? allPages.length + 1 : undefined;
-      },
-      staleTime: 24 * 60 * 60 * 1000 // 24hr
-          
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam,
+        },
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage ? allPages.length + 1 : undefined;
+    },
+    staleTime: ms("24h"),
   });
 
 export default useGames;
