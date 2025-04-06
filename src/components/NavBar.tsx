fcd5dfddd4c9296dcd04b1c11e7485c9c37cfcdc
Nam Pham <nampham@Nams-MacBook-Air.local>
2025-04-06 16:55:17 +0300

    fix NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,19 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/bulls-eye.webp'
-import ColorModeSwitch from './ColorModeSwitch'
-import SearchInput from './SearchInput'
-
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/bulls-eye.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <HStack padding='10px'>
-        <Image src={logo} boxSize='60px'>
-        </Image>
-        <SearchInput />
-        <ColorModeSwitch></ColorModeSwitch>
+    <HStack padding="10px">
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit={'cover'}/>
+      </Link>
+      <SearchInput />
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
